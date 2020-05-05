ec61382bb5c6133cd339633f54ab4da3589fee36
gjyhuang <grace.jy.huang@gmail.com>
2020-05-05 01:24:43 -0400

    add: mobile + desktop nav layout/icons

---
+++ src/components/nav/NavDesktop.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavDesktop = () => {
+  return (
+    <div className="nav--desktop">
+      <ul>
+        <li className="nav__logo--desktop">
+          <Link to="/">NookAtMe</Link>
+        </li>
+        <li>
+          Catalog
+        </li>
+        <li>
+          Checklists
+        </li>
+        <li>
+          Community
+        </li>
+        <li>
+          Profile
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavDesktop;
